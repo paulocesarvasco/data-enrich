98fa69892ed04da421a35cce6e7a0a76f99769aa
Paulo Cesar <paulo.souza@kryptus.com>
2022-01-27 02:29:45 -0300

    Create funtion to get region from country name

--- main.go
+++ main.go
@@ -28,4 +28,43 @@ func main() {
 	}
 	fmt.Printf("%+v\n", records) // output for debug
 
+
+func getCountryRegion(country string) string {
+
+	i := 0
+	for {
+		regionName := constants.Region(i).String()
+		if regionName == "" {
+			fmt.Printf("%+v\n", "Região não encontrada") // output for debug
+			return ""
+		}
+		url := "https://restcountries.com/v3.1/region/" + regionName
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("%+v\n", err) // output for debug
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("%+v\n", err) // output for debug
+		}
+
+		var countryList []models.Country
+		// Unmarshal request body
+		err = json.Unmarshal(body, &countryList)
+		if err != nil {
+			fmt.Printf("%+v\n", err) // output for debug
+		}
+
+		defer resp.Body.Close()
+
+		for _, countryInfo := range countryList {
+			if country == countryInfo.Name.Common {
+				return countryInfo.Region
+			}
+		}
+
+		i++
+	}
 }
