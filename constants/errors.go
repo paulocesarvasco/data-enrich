53360c1298e15a651ef01499e4be62b29eb5c4ee
Paulo Cesar <paulo.souza@kryptus.com>
2022-01-30 21:52:45 -0300

    Define errors messages

---
+++ constants/errors.go
@@ -0,0 +1,21 @@
+package constants
+
+var ErrorToCreateDatabaseInstace = "Failed to create database instance"
+var ErrorToCreateDatabaseCollection = "Failed to create collection on database"
+
+var ErrorToUnmarshallRequestBody = "Failed to decode http request body"
+var ErrorToRetriveCountryFromIp = "Failed to get country name from external API"
+var ErrorToRetriveRegionName = "Failed to get country region name"
+var ErrortoSaveDataOnDatabase = "Failed to store new record in the database"
+var ErrortoGetRecordsFromDatabase = "Couldn't retrieve data from database"
+var ErrortoEncodeDataFromDatabase = "Failed to encode data retrieved from database"
+
+var ErrorToEstablishDatabaseConnection = "Couldn't establish connection with database service"
+var ErrorTestingConnectionWithDB = "Database connection not responding"
+var DatabaseClientNotInitialized = "Database client not initialized"
+var CollectionNotFound = "Couldn't find the collection"
+var ErrorToRetrieveRecordsFromDb = "Failed when trying to retrieve data from the database"
+
+var ErrorToRetrieveDataFromUri = "Unable to retrieve data from URI"
+var FailToReadHttpResponseBody = "Couldn't read data from http response"
+var RegionNotFound = "Unknown region"
