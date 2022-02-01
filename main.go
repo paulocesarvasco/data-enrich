38e4eaa4783c723c6cba941c94d365cf1cf2b59b
Paulo Cesar <paulo.souza@kryptus.com>
2022-02-01 03:41:46 -0300

    Fix url

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	var err error
 
-	err = database.CreateDatabaseInstance("mongodb://127.0.0.1:27017")
+	err = database.CreateDatabaseInstance("mongodb://mongodb:27017")
 	if err != nil {
 		log.Fatal(utils.WrapError(err, cte.ErrorToCreateDatabaseInstace))
 	}
