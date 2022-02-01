67025faeca2e05cffdb252d491c12c495f3d5302
Paulo Cesar <paulo.souza@kryptus.com>
2022-02-01 03:41:33 -0300

    Fix message

--- constants/messages.go
+++ constants/messages.go
@@ -3,4 +3,4 @@ package constants
 var ConnectionWithDbEstablish = "Connection with database service establish"
 var StartingServer = "Starting server..."
 var DataSavedWithId = "Data saved. Record ID: "
-var DataRetrieved = "Data retrieved from "
+var DataRetrieved = "Data retrieved by "
