03f4d0887fd2ddbb4accfc0b3e5eea46c636ed9a
Paulo Cesar <paulo.souza@kryptus.com>
2022-02-01 03:15:17 -0300

    Create info messages

---
+++ constants/messages.go
@@ -0,0 +1,6 @@
+package constants
+
+var ConnectionWithDbEstablish = "Connection with database service establish"
+var StartingServer = "Starting server..."
+var DataSavedWithId = "Data saved. Record ID: "
+var DataRetrieved = "Data retrieved from "
