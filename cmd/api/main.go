ed21bc5304e3c60fec25fadb65ee66e53be7617e
paulocesarvasco <paulocvasco.93@gmail.com>
2024-10-07 20:55:47 +0200

    Reflect changes on main function

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,27 +5,12 @@ import (
 	"net/http"
 
 	cte "data-enrich/internal/constants"
-	"data-enrich/internal/database"
 	"data-enrich/internal/engine"
-	"data-enrich/internal/utils"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-
-	var err error
-
-	err = database.CreateDatabaseInstance("mongodb://mongodb:27017")
-	if err != nil {
-		log.Fatal(utils.WrapError(err, cte.ErrorToCreateDatabaseInstace))
-	}
-
-	err = database.CreateDatabaseCollection("CloudtrailRecords", "records")
-	if err != nil {
-		log.Fatal(utils.WrapError(err, cte.ErrorToCreateDatabaseCollection))
-	}
-
 	enrich := engine.New()
 	r := mux.NewRouter().StrictSlash(true)
 
